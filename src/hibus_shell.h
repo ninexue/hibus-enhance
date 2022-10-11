d7ddfcb6f02ff6cc333acb40c5e29949c678ce3a
h2005105 <h2005105@sina.com>
2022-10-11 13:07:29 +0800

    add src

--- src/hibus_shell.h
+++
@@ -1,7 +0,0 @@
-#ifndef __INETD_DEVICE_COMMON__
-#define __INETD_DEVICE_COMMON__
-
-void common_register(hibus_conn * hibus_context_inetd);
-void common_revoke(hibus_conn * hibus_context_inetd);
-
-#endif  // __INETD_DEVICE_COMMON__
